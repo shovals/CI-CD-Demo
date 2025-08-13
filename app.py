c9d721c09b857b0cb2b08a8875e68bbe22910d5e
shoval <shoval.shalev@gmail.com>
2025-08-13 10:01:45 +0300

    Added another function to main and a unittest for it

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ def multiply(a, b):
     # This function multiply 2 numbers
     return a * b
 
+def subtract(a, b):
+    # This function subtract 2 numbers
+    return a-b
+
+
 if __name__ == "__main__":
     print("Add:", add(2, 3))
     print("Multiply:", multiply(2, 3))
+    print("Subtract:", subtract(10,7))
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,16 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import add, multiply
+from app import *
 
 def test_add():
     assert add(2, 3) == 5
+    assert add(7,3) == 10
 
 def test_multiply():
     assert multiply(2, 3) == 6
+    assert multiply(3,4) == 12
+
+def test_subtract():
+    assert subtract(5,2) == 3
+    assert subtract(15, 10) == 5
